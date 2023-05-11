c2d420a0c7b94e7ac9a970f8b7eb9a7cba3b9d57
LuisDanielD4757575 <119696460+LuisDanielD4757575@users.noreply.github.com>
2023-05-11 11:18:08 -0700

    Add files via upload

---
+++ EVA3_6_TRIGGERS2.sql
@@ -0,0 +1,16 @@
+delimiter //
+create trigger datos_previos_update_per after update on personas 
+for each row
+begin
+	insert into auditoria_per 
+    values(
+		old.id,
+        old.nombre,
+        old.apellido_pat,
+        old.apellido_mat,
+        old.fecha_nac,
+        old.clave_rfc
+    );
+end //
+
+delimiter ;
